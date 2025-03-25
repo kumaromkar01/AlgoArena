8b6ae5e039207ee6ba71df212c840a613949114f
Omkar Kumar <kumaromkar1311@gmail.com>
2025-03-25 18:01:15 +0530

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const { 
+  signUp, 
+  login, 
+  createProj, 
+  saveProject, 
+  getProjects, 
+  getProject, 
+  deleteProject, 
+  editProject 
+} = require('../controllers/userController');
+var router = express.Router();
+
+router.post("/signUp", signUp);
+router.post("/login", login); 
+router.post("/createProj", createProj); 
+router.post("/saveProject", saveProject); 
+router.post("/getProjects", getProjects); 
+router.post("/getProject", getProject); 
+router.post("/deleteProject", deleteProject); 
+router.post("/editProject", editProject);
+
+module.exports = router;
